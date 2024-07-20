59c4329fca5eabfe956b7007924998bfea7f247c
Pavithra2005Mohan <pavi261105@gmail.com>
2024-07-20 09:24:45 +0530

    pavdhar

--- nodemailer-gmail/mail.js
+++ nodemailer-gmail/mail.js
@@ -1,34 +1,37 @@
 const nodemailer = require('nodemailer');
+
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-    auth: {
-      user: 'pavithra.web.developer@gmail.com',
-      pass: 'hjhn xyse toyv otrf'
-    }
+  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  tls: {
+    ciphers: 'SSLv3'
+  },
+  auth: {
+    user: process.env.GMAIL_USERNAME,
+    pass: process.env.GMAIL_PASSWORD
+  }
 });
 
-// send mail with defined transport object
 function sendMail(formData) {
-    let mailOptions = {
-        from: formData.email,
-        to: 'pavi261105@gmail.com',
-        subject: 'New Contact Form Submission',
-        text: `Name: ${formData.name}\nEmail: ${formData.email}\nWebsite: ${formData.website}\nMessage: ${formData.message}`,
-        html: `<p>Name: ${formData.name}</p><p>Email: ${formData.email}</p><p>Website: ${formData.website}</p><p>Message: ${formData.message}</p>`
-    };
+  const mailOptions = {
+    from: formData.email,
+    to: 'pavi261105@gmail.com',
+    subject: 'New Contact Form Submission',
+    text: `Name: ${formData.name}\nEmail: ${formData.email}\nWebsite: ${formData.website}\nMessage: ${formData.message}`,
+    html: `<p>Name: ${formData.name}</p><p>Email: ${formData.email}</p><p>Website: ${formData.website}</p><p>Message: ${formData.message}</p>`
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Message sent: %s', info.messageId);
-        }
-    });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
 }
 
 module.exports = {
-    sendMail
+  sendMail
 };
